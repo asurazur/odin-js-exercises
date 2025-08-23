64fa4c906e66cb1c661a8691f656a7de9eca3a50
asurazur <asurazur@gmail.com>
2025-08-23 22:34:26 +0800

    feat: add solution to 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof(a) != 'number' || typeof(b) != 'number')
+        return 'ERROR'
+    if (a < 0 || b < 0)
+        return 'ERROR'
+    if (a % 1 != 0 || b % 1 != 0)
+        return 'ERROR'
+    if (a > b) {
+        a = a + b
+        b = a - b
+        a = a - b
+    }
+    let n = b - a + 1
+    return n * ((a + b)/2)
 };
 
 // Do not edit below this line
