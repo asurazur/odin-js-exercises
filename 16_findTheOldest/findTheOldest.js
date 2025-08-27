68bc4af28a6da761993cc69c325266c97ce11ca5
asurazur <asurazur@gmail.com>
2025-08-27 10:03:40 +0800

    feat: add solution to 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+function calculateAge(obj) {
+    if (!('yearOfDeath' in obj))
+        obj.yearOfDeath = new Date().getFullYear()
+    return obj.yearOfDeath - obj.yearOfBirth
+}
 
+const findTheOldest = function(arr) {
+    return arr.reduce((oldestPerson, currentPerson) => {
+        return (calculateAge(oldestPerson) > calculateAge(currentPerson)) ? oldestPerson : currentPerson
+    }, {})
 };
 
 // Do not edit below this line
