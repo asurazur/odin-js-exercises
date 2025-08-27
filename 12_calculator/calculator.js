7586cf926ccf3a1d8189fcb98455061b6fa440ec
asurazur <asurazur@gmail.com>
2025-08-27 08:36:10 +0800

    feat: add solution to 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => {return total += current}, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total *= current)
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  if (y == 1) {
+    return x
+  }
+	return x * power(x, y-1)
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x == 0) {
+    return 1
+  } 
+  return x * factorial(x - 1)
 };
 
 // Do not edit below this line
