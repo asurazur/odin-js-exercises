cc9e6f4a1e43c78dd1ae198b585d6bbe95170712
asurazur <asurazur@gmail.com>
2025-08-27 09:37:34 +0800

    feat: add solution to 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+function isAlphabet(ch) {
+    return /^[a-zA-Z]$/.test(ch);
+}
 
-};
+const palindromes = function (text) {
+    text = text.toLowerCase()
+    text = text.replace(/[^a-z0-9]/g, "");
+    let [left, right] = [0, text.length-1]
+    while(left < right){
+        if(text[left] != text[right]){
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+}
 
 // Do not edit below this line
 module.exports = palindromes;
